2d8cbcd1e6b9d03dbcf523263c68ece97372c078
jenhil0809 <jenhil0809@highgateschool.org.uk>
2023-11-23 15:57:27 +0000

    2019 day1 part 2

--- 2019day1.py
+++ 2019day1.py
@@ -1,9 +1,12 @@
 lines = []
 total = 0
-#part 1
 with open("day1.txt", "r") as file:
     for line in file:
         lines.append(int(line.strip()))
 for val in lines:
     total += val//3-2
+    fuel_add = val//3-2
+    while fuel_add//3-2 > 0:
+        total += fuel_add//3-2
+        fuel_add = fuel_add//3-2
 print(total)
