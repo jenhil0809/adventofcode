26a77c1bb079a0a2acd94dc6109511c2b98e0ec3
jenhil0809 <jenhil0809@highgateschool.org.uk>
2023-11-23 15:41:52 +0000

    2019 day1

--- 2019day1.py
+++ 2019day1.py
@@ -1,9 +1,9 @@
 lines = []
 total = 0
+#part 1
 with open("day1.txt", "r") as file:
     for line in file:
         lines.append(int(line.strip()))
 for val in lines:
     total += val//3-2
 print(total)
-#part 1
