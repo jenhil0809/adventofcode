f7ee96d16e9dc2645a7e22617da15a62f7a4fb02
jenhil0809 <jenhil0809@highgateschool.org.uk>
2023-12-18 10:37:11 +0000

    2023 day 12

---
+++ 2023day12.py
@@ -0,0 +1,42 @@
+from functools import cache
+
+with open("2023day12.txt", "r") as file:
+    puzzle_input = [line for line in file]
+
+
+@cache
+def ways(sequence, groups):
+    if not groups:
+        return '#' not in sequence
+    seq_len = len(sequence)
+    group_len = groups[0]
+    if seq_len - sum(groups) - len(groups) + 1 < 0:
+        return 0
+    has_holes = any(sequence[x] == '.' for x in range(group_len))
+    if seq_len == group_len:
+        return 0 if has_holes else 1
+    can_use = not has_holes and (sequence[group_len] != '#')
+    if sequence[0] == '#':
+        return ways(sequence[group_len + 1:].lstrip('.'), tuple(groups[1:])) if can_use else 0
+    skip = ways(sequence[1:].lstrip('.'), groups)
+    if not can_use:
+        return skip
+    return skip + ways(sequence[group_len + 1:].lstrip('.'), tuple(groups[1:]))
+
+
+total = 0
+for line in puzzle_input:
+    sequence, groups = line.split()
+    groups = [int(g) for g in groups.split(',')]
+    total += ways(sequence, tuple(groups))
+
+print(total)
+
+total = 0
+for line in puzzle_input:
+    sequence, groups = line.split()
+    sequence = '?'.join([sequence] * 5).lstrip('.')
+    groups = [int(g) for g in groups.split(',')] * 5
+    total += ways(sequence, tuple(groups))
+
+print(total)
